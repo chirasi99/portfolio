fa64cdd495d51652af575ab35583ccb813bc0170
Chirasi Amaya Walpola <chirasiamaya99@gmail.com>
2024-06-21 17:59:53 +0530

    Update RightBanner.js

--- src/components/banner/RightBanner.js
+++ src/components/banner/RightBanner.js
@@ -5,11 +5,11 @@ const RightBanner = () => {
   return (
     <div className="relative flex items-center justify-center w-full lgl:w-1/2">
       <img
-        className="w-[300px] h-[400px] lgl:w-[500px] lgl:h-[680px] z-10"
+        className="w-[200px] h-[200px] lgl:w-[450px] lgl:h-[500px] z-10"
         src={bannerImg}
         alt="bannerImg"
   />
-      <div className="absolute bottom-0 w-[310px] h-[280px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
+      // <div className="absolute bottom-0 w-[310px] h-[280px] lgl:w-[500px] lgl:h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
     </div>
   );
 }
